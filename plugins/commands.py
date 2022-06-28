a2fe4d6b4d880704f77239f387afa44d842830ca
Kk2020k <108136211+Kk2020k@users.noreply.github.com>
2022-06-28 09:27:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -230,7 +230,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Update', url='https://t.me/Update20k') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
